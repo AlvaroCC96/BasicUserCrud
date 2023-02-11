6223f5dfc36e0933fc6f344e792b6eac666c78dc
Alvaro Castillo <alvarolucascc96@gmail.com>
2023-02-10 22:03:41 -0300

    add: user schema for db

---
+++ backend/src/models/User.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: String,
+    lastname: String,
+    age: Number,
+    phone: Number,
+    email: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("User", userSchema);
