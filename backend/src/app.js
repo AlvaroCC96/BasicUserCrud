ebce413c835253eaa701f29007d5a3ee649ceecb
Alvaro Castillo <alvarolucascc96@gmail.com>
2023-02-10 22:02:14 -0300

    add: main with routes, middlewares and configurations

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// Configuration
+app.set("port", process.env.PORT || 4000);
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to APIREST");
+});
+
+// Api route
+app.use('/api/users', require('./routes/user'))
+module.exports = app;
